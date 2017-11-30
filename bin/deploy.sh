ab26ee3dfbc85547f9274366701e2433d1cbdb5d
黄诚就 <huangcj@iaskdoc.com>
2017-11-30 11:39:51 +0800

    更改bin下的执行打包脚本

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,22 +3,24 @@
 #脚本demo
 serverStaticPath="fun:~/test-scp/"
 
-cd ../src
+cd ../
+
+echo "please enter env(dev/test/prod):"
+read -r env
 
 rm -rf build/
+npm run "$env"
 
-# 实际上还需要让打包者输入分支再进行打包
+if [ "$env" = "test" ];then
+  echo "send to test-server?(yes or no)"
+  read -r result
 
-echo "开始打包"
-npm run prod
+  if [ "$result" = "yes" ];then
+    echo "开始发送"
+    # cd ../build
+    # scp -r * $serverStaticPath
+  else
+    exit -1
+  fi
 
-echo "send to test-server?(yes or no)"
-read -r result
-
-if [ "$result" = "yes" ];then
-  echo "开始发送"
-  cd ../build
-  scp -r * $serverStaticPath
-else
-  exit -1
 fi
